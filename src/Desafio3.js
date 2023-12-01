ddb7fc7df1d300dcacba6da5f1ef5d5a75db896a
miguellcapistrano <luizmiguell9322@gmail.com>
2023-12-01 11:50:57 -0300

    Escrevendo as Classes de um Jogo

---
+++ src/Desafio3.js
@@ -0,0 +1,29 @@
+//Desafio Escrevendo as Classes de um Jogo
+class meuHeroi{
+	constructor(nome, idade, tipo){
+    	this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+	}
+    atacar(){
+    	let ataque = ""
+		switch (this.tipo) {
+    			case "mago":
+        		ataque = " usou magia";
+                break
+        		case "guerreiro":
+        		ataque = " usou espada";
+                break
+                case "monge":
+        		ataque = " usou artes marciais";
+                break
+                case "ninja":
+        		ataque = " usou shuriken";
+                break
+              }        
+		console.log("O " + this.tipo + ataque)
+    }
+}
+
+let meuMago = new meuHeroi("Miguell", 30, "mago")
+meuMago.atacar()
