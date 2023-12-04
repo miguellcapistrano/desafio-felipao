f4420fdbf326f6f258a60e60a2c35c0e5c001928
miguellcapistrano <luizmiguell9322@gmail.com>
2023-12-04 11:58:30 -0300

    Desafios atualizados

--- Desafios DIO prog/Desafio3.js
+++ Desafios DIO prog/Desafio3.js
@@ -8,19 +8,19 @@ class meuHeroi{
     atacar(){
     	let ataque = ""
 		switch (this.tipo) {
-    			case "mago":
-        		ataque = " usou magia";
-                break
-        		case "guerreiro":
-        		ataque = " usou espada";
-                break
-                case "monge":
-        		ataque = " usou artes marciais";
-                break
-                case "ninja":
-        		ataque = " usou shuriken";
-                break
-              }        
+    		case "mago":
+        	ataque = " usou magia";
+            break
+        	case "guerreiro":
+        	ataque = " usou espada";
+            break
+            case "monge":
+        	ataque = " usou artes marciais";
+            break
+            case "ninja":
+        	ataque = " usou shuriken";
+            break
+        }        
 		console.log("O " + this.tipo + ataque)
     }
 }
