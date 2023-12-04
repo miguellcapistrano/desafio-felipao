9f7e1fdbfad742002c4308232b41739115b3e259
miguellcapistrano <luizmiguell9322@gmail.com>
2023-12-04 11:56:13 -0300

    Desafio1 alt

--- Desafios DIO prog alt/Desafio1 alt.js
+++ Desafios DIO prog alt/Desafio1 alt.js
@@ -1,4 +1,5 @@
-// Definindo o nome e a quantidade de experiência 
+//usando if e else if 
+//Definindo o nome e a quantidade de experiência 
 let nomeDeHeroi = "Jacinto";
 let xp = 7500;
 
@@ -6,33 +7,29 @@ let xp = 7500;
 
 let nivel; 
 
-while (true) {
-	switch (true) {
-    	case (xp < 1000):
+if (xp < 1000){
         nivel = "Ferro";
-        break
-        case (xp >= 1001 && xp <= 2000):
-        nivel = "Bronze";
-        break
-        case (xp >= 2001 && xp <= 5000):
-        nivel = "Prata";
-        break
-        case (xp >= 5001 && xp <= 7000):
-        nivel = "Ouro";
-        break
-        case (xp >= 7001 % xp <= 8000):
-        nivel = "Platina";
-        break
-        case (xp >= 8001 && xp <= 9000):
-        nivel = "Ascendente";
-        break
-        case (xp >= 9001 && xp <= 10000):
-        nivel = "Imortal";
-        break
-        case (xp >= 10001):
-        nivel = "Radiante";
-        break;
-        }
-        break}
+}
+else if (xp >= 1001 && xp <= 2000){
+        nivel = "Bronze"
+}
+else if (xp >= 2001 && xp <= 5000){
+	nivel = "Prata"
+}
+else if (xp >= 5001 && xp <= 7000){
+	nivel = "Ouro"
+}
+else if(xp >= 7001 % xp <= 8000){
+	nivel = "Platina"
+}
+else if (xp >= 8001 && xp <= 9000){
+	nivel = "Ascendente"
+}
+else if(xp >= 9001 && xp <= 10000){
+	nivel = "Imortal"
+}
+else if (xp >= 10001){
+	nivel = "Radiante" 
+}
         
 console.log("O herói de nome " + nomeDeHeroi + " está no nível de " + nivel)
